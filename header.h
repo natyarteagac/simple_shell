dc16a7e478a98d1675918bf2e0011bc4a96e4a4a
natyarteagac <natalia.arteagac94@gmail.com>
2021-04-13 16:48:41 -0500

    header

---
+++ header.h
@@ -0,0 +1,18 @@
+#ifndef HEADER
+#define HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+#define _XOPEN_SOURCE 600
+
+
+char **super_array(char *buffer, char *deli);
+int main(void);
+int execute(char **comandos);
+
+#endif
