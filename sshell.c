671f29646beb21da17b5b0c15ba7a0d9a7acf9da
luiscolorado-tech <luiscolorado@outlook.com>
2021-04-13 22:21:03 -0500

    Fix sshel

--- sshell.c
+++ sshell.c
@@ -8,32 +8,36 @@
 int main(void)
 {
 	int j = 0;
-	int i = 0;
 	
+	
+    int userline;
 	char **arraytoken = NULL;
 	char *buffer = NULL;
 	size_t buffersize = 0;
-	
-
-	write(1, "#cisfun$ ", 9);
-
+    pid_t my_pid;
+    
+	write(1, "#cisfun$ ", 9); 
+	 
 	while (getline(&buffer, &buffersize, stdin) != EOF)
 	{
+		
 		while (buffer[j] != 0)
 		{
 			if (buffer[j] == '\n')
-			{
-				buffer[j] = 0;
-				j++;
-			}
+			buffer[j] = 0;
+			j++;
 		}
-		arraytoken = super_array(buffer, " ");
-		i = 0;
-
-		/*status1 = status(1, *arraytoken);*/
-
+        arraytoken = super_array(buffer, " ");
+		
+		
+		//status1 = status(1, *arraytoken);
+		//execute(arraytoken);
+		 
 		execve(arraytoken[0], arraytoken, NULL);
+		
 	}
+	
 	return (0);
+
 }
 
